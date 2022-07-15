0b6e22493519cddf4538c30ad6834c941c612447
Ajay KC <meajay64@gmail.com>
2022-07-16 00:41:36 +0900

    Solve close btn hidin bug
    Close btn did not showed in small screen. There was 'style' instead of 'classlist'. Now it is working fine.

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ logo.addEventListener('click', () => {
     sidebar.classList.toggle('show');
     sidebar.style.left = '0px';
 
-    logo.style.display = 'none';
-    closeBtn.classList.toggle('show')
+    logo.classList.toggle('show');
+    closeBtn.classList.toggle('show');
 })
 
 closeBtn.addEventListener('click', () => {
     sidebar.classList.toggle('show');
     sidebar.style.left = '-1000px';
 
-    logo.style.display = 'block';
-    closeBtn.classList.toggle('show')
+    logo.classList.toggle('show');
+    closeBtn.classList.toggle('show');
 })
