9056174c91a33e6a5f92207ba1f1367a66a7ba80
Ajay KC <meajay64@gmail.com>
2022-07-16 09:48:10 +0900

    Give full height of sidebar equal to html element

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let logo =  document.querySelector('.logo');
 let closeBtn = document.querySelector('.close-btn');
 
 logo.addEventListener('click', () => {
+    let html = document.querySelector('html');
+    sidebar.style.height = html.offsetHeight; 
     sidebar.classList.toggle('show');
     sidebar.style.left = '0px';
 
